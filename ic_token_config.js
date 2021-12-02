30d6203a7f8770639e01a28eecd233b7cdc05c01
Adam Stępnik <adam@excodus.com>
2021-12-03 00:53:49 +0100

    Signature storage

--- ic_token_config.js
+++ ic_token_config.js
@@ -1,10 +1,6 @@
 // GET YOUR INFURA API ENDPOINT FROM https://infura.io/
 module.exports.ENDPOINT = "http://127.0.0.1:8000"
-module.exports.ADDRESS = "rwlgt-iiaaa-aaaaa-aaaaa-cai"
-
-//Used for signing messages for the vault
-module.exports.privateKey = "f8b02e6eb8e99487b2a5baa406d854f57509cc5472c92069a9880c41d844067b";
-module.exports.publicAddress = "0x800D04094a14B44D678181eA8B8399BFA030Fea1";
+module.exports.ADDRESS = "rrkah-fqaaa-aaaaa-aaaaq-cai"
 
 // Mumbai testnet
 // module.exports.ENDPOINT = "https://matic-mumbai.chainstacklabs.com"
