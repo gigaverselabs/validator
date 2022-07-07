445d44c443bd8bab59940f71a791acb59a72c5f8
stopak <1740874+stopak@users.noreply.github.com>
2022-07-07 14:47:07 +0200

    Added key trimming

--- block_scanner/evm_validator.js
+++ block_scanner/evm_validator.js
@@ -35,7 +35,7 @@ const signature_vault = Actor.createActor(signature_vault_idl.IDL, {
     canisterId: config.IC_SIG_CANISTER,
 });
 
-const privateKey = fs.readFileSync('../eth_key');
+const privateKey = fs.readFileSync('../eth_key').trim();
 const account = web3.eth.accounts.privateKeyToAccount('0x' + privateKey);
 console.log("Using ETH Validator: "+account.address);
 
